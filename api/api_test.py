dc5045e084e6d6525e8bf59c46a6d58f7010c6e8
Nadir <yusipov.nadir@gmail.com>
2022-07-15 14:51:12 +0300

    переход по аватарке в html-файлах исправлен, добавлены тесты для api

---
+++ api/api_test.py
@@ -0,0 +1,17 @@
+import pytest
+from run import app
+
+keys_should_be = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count", "pk"}
+
+
+def test_api_posts():
+    response = app.test_client().get('/api/posts/')
+    assert response.status_code == 200, "Неверный статус-код"
+    assert len(response.json) > 0, "Пустой список"
+    assert set(response[0].json.keys()) == keys_should_be
+
+
+def test_api_single_post():
+    response = app.test_client().get('/api/posts/1')
+    assert response.status_code == 200, "Неверный статус-код"
+    assert len(response.json) == 1, "Возвращается не один пост"
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from posts.views import posts_blueprint
-from api.api import api_blueprint
+from api.api_blueprint import api_blueprint
 
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 app.register_blueprint(posts_blueprint)
 app.register_blueprint(api_blueprint)
